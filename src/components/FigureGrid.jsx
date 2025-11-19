e8b99ccdf5096eec15219cdc451951360d6087ef
denisseram <a01562077@tec.mx>
2025-11-19 07:24:13 +0100

    Revert "a ver"
    This reverts commit b9a81d849777593d9b464c0fea5241fd68889206.

--- src/components/FigureGrid.jsx
+++ src/components/FigureGrid.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../star-spatialsc/styles/FigureGrid.css";
+import "../styles/FigureGrid.css";
 
 export default function FigureGrid({ figures }) {
   const [selectedCodes, setSelectedCodes] = useState([]);
